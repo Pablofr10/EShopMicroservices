f43a2c51196bac07cad503b6919705136907e531
Pablo Rafael <rafanet.live@gmail.com>
2024-04-22 21:15:33 -0300

    Registring Cater and Mediator

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,7 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssemblies(typeof(Program).Assembly);
+});
 
 var app = builder.Build();
+
+app.MapCarter();
 
 app.Run();
